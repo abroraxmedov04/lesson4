347d40f3b61e8690411b7850882ae1607cd3752d
abroraxmedov04 <abrordevaxmedov@gmail.com>
2023-10-11 16:46:41 +0300

    first-commit

---
+++ main.js
@@ -0,0 +1,58 @@
+// todo: lesson 4 homework
+
+// ! for 1
+// function myFunc() {
+//   let k = prompt("enter the number"); /*smth that shall repeat several time */
+//   let n = prompt("enter the second number");
+//   if (n > 0) {
+//     for (i = 0; i <= n; i++) {
+//       console.log(k, i);
+//     }
+//   }
+// }
+// myFunc();
+
+// ! for 2
+// function myFunc() {
+//   let a = prompt();
+//   let b = prompt();
+//   if (a < b) {
+//     for (let i = b; i <= a; i++) {
+//       console.log(a);
+//     }
+//   }
+// }
+// myFunc();
+// todo
+
+// ! for 3
+// a = prompt();
+// b = prompt();
+// for (let i = b - 1; i > a; i--) {
+//   console.log(i);
+// }
+// let chiqarilganSonlarSoni = b - a - 1;
+// console.log("chiqarilgan sonlarni soni:", chiqarilganSonlarSoni);
+
+// ! for 4
+// let price = parseInt(prompt("narhni kiriting"));
+// for (i = 1; i <= 10; i++) {
+//   res = price * i;
+//   console.log(res);
+// }
+
+// ! for 5
+// let a = parseInt(prompt("enter the number"))
+// for (let i=0.1; i<=0.9; i++){
+//   res= a * i;
+//   console.log(res);
+// }
+
+// ! for 6
+// let a = parseInt(prompt("enter the number"));
+// for (var i = 1.1; i <= 9.9; i++) {
+//   res = a / i;
+//   console.log(i);
+// }
+
+
